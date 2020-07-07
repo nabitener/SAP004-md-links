11f590e257c0011cc54b5306458815f2ef662acf
nabitener <na_bitener@hotmail.com>
2020-07-07 17:07:04 -0300

    Lendo e retornando links de diretorio

--- index.js
+++ index.js
@@ -1,35 +1,55 @@
 const fs = require("fs");
 const path = require("path");
 
-mdLinks = (file) => {
-  const extension = path.extname(file);
+const textLink = /[^!]\[(.[^\]]*)\]/;
+const hrefLink = /\(([^#]\S+)\)/;
+const regex = /[^!]\[(.[^\]]*)\]\(([^#]\S+)\)/gm;
+const arrayLinks = [];
 
+mdLinks = (file) => {
+  readFile(file);
+};
+
+readDiretory = (path1) => {
   return new Promise((resolve, reject) => {
-    fs.readFile(file, "utf8", (error, data) => {
-      if (error || extension !== ".md") {
-        reject(error.message);
+    fs.readdir(path1, "utf8", (error, files) => {
+      if (error) {
+        reject(console.log(error));
       } else {
-        resolve(formArray(file));
+        files.forEach((file) => {
+          if (path.extname(file) === ".md") {
+            resolve(readFile(file));
+          }
+        });
       }
     });
   });
 };
 
-formArray = (file) => {
-  const textLink = /\[\w*\]/;
-  const hrefLink = /\(([^#]\S+)\)/gm;
-  const regex = textLink + hrefLink;
-  const arrayLinks = [];
+readFile = (file) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, "utf8", (error, data) => {
+      if (error || path.extname(file) !== ".md") {
+        reject(error);
+      } else {
+        resolve(formArray(file, data));
+      }
+    });
+  });
+};
 
+formArray = (file, data) => {
   const links = data.match(regex);
   links.forEach((index) => {
     const text = index.match(textLink)[0];
     const href = index.match(hrefLink)[1];
     arrayLinks.push({ file, href, text });
   });
-  return arrayLinks;
+  return console.log(arrayLinks);
 };
 
 //mdLinks('./README.md');
+
+readDiretory("./");
 
 module.exports = mdLinks;
